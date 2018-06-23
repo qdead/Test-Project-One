8bec0c85cdd33f96366cb1e65841bf05180c672f
qdead <william.hoglund@live.se>
2018-06-23 07:26:48 +0200

    Trying to remeber what everything does.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Creates an array with values already added.
+            double[] doubleArray = { 1.05, 7, 1.3, 8, 9.65, 4, 9};
+            //Creates an empty array.
+            double[] newArray = new double[4];
+            //Adds value to my empty array
+            newArray[0] = 3;
+            newArray[1] = 2.3;
+            newArray[2] = 9.7;
+            newArray[3] = 5.6;
+            //For loop that loops through my array a specific amount of items.
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(newArray[i]);
+            }
+            //for loop that loops through all my item's-
+            foreach (var item in doubleArray)
+            {
+                Console.WriteLine(item);
+            }
             Console.WriteLine("Test");
             Console.ReadKey();
         }
